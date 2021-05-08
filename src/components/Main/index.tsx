9eb102ba23db87f3bd20d928382d0ab640e12f72
assoares383 <alexandressoares383@gmail.com>
2021-05-08 15:02:29 -0300

    first commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,17 +1,3 @@
-import * as S from "./styles";
-
-const Main = () => (
-    <S.Wrapper>
-        <S.Logo src="/images/logo.svg" alt="Imagem de um atomo" />
-        <S.Title>React Avançado</S.Title>
-        <S.Description>
-            Typescript, ReactJS, NextJS e Styled Components
-        </S.Description>
-        <S.Illustration
-            src="/images/hero-illustration.svg"
-            alt="Um desenvolvedor de frente para uma tela com codigo."
-        />
-    </S.Wrapper>
-);
+const Main = () => <h1>Hello World</h1>;
 
 export default Main;
